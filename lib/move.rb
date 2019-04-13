c1dcbe16d8eabac021e1c0adac4004317bc2fd99
Ranika Williams <rnkprice07@gmail.com>
2019-04-13 18:50:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
   new_user_input = user_input.to_i
   new_user_input -= 1
   return new_user_input
-end 
+end
 
 def move(board, index, character = "X")
   board[index] = character
